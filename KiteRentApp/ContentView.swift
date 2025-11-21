5a4ecf910037739965ec93031eecde93a3a1d52c
H3LLY3A1 <koblazej502@gmail.com>
2025-11-21 19:15:07 +0100

    Initial Commit

---
+++ KiteRentApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KiteRentApp
+//
+//  Created by Ranger5301 on 21/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KiteRentApp/KiteRentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiteRentAppApp.swift
+//  KiteRentApp
+//
+//  Created by Ranger5301 on 21/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct KiteRentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
