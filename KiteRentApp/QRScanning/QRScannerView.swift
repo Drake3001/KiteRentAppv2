8eda097097c7811969e4bc7dc7bcbfa5970180c6
H3LLY3A1 <koblazej502@gmail.com>
2025-11-23 21:10:46 +0100

    qrscanner

---
+++ KiteRentApp/QRScanning/QRScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  QRScannerView.swift
+//  KiteRentApp
+//
+//  Created by Ranger5301 on 23/11/2025.
+//
+
+import SwiftUI
+
+struct QRScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRScannerView()
+}
---
+++ KiteRentApp/QRScanning/ScanKiteView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanKiteView.swift
+//  KiteRentApp
+//
+//  Created by Ranger5301 on 23/11/2025.
+//
+
+import SwiftUI
+
+struct ScanKiteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanKiteView()
+}
